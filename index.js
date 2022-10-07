68e226c548c94788dcd8800cb036372621ba8cca
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-10-07 14:53:03 +0200

    fetch movies, data check

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('Hello world');
-    res.send('Hello world!');
+app.get('/', async (req, res) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: "891d1ad5",
+            s: "dark"
+        }
+    });
+    
+    const data = response.data.Search;
+
+    const renderedMovies = data.map(movie => {
+        return `
+            <div>${movie.Title}</div>
+        `;
+    }).join('');
+
+    res.send(renderedMovies);
 });
 
 app.listen('3002', () => {
