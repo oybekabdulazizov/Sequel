86a6a2ca398fefefc320b4fafffbb93311e9c070
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-12-15 19:01:12 +0100

    rename the indexmovie.js to index.js

--- routes/movie/movies.js
+++ routes/movie/movies.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const moviesRepo = require('../../repositories/movies');
-const moviesIndexTemplate = require('../../views/movie/indexmovie');
+const moviesIndexTemplate = require('../../views/movie/index');
 
 router.get('/movies', async (req, res) => {
     const movies = await moviesRepo.getAll();
