305c2d906e70bdb0b1cf3131b880560ac3fca6f7
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-12-18 19:44:31 +0100

    add Price attribute to each movie and update the JSON data file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cookieSession = require('cookie-session');
+const fs = require('fs');
 
 const authRouter = require('./routes/auth/auth');
 const moviesRouter = require('./routes/movie/movies');
@@ -17,6 +18,24 @@ app.use(authRouter);
 app.use(moviesRouter);
 app.use(homepageRouter);
 app.use(cartsRouter);
+
+
+app.get('/test', async (req, res) => {
+    const movies = JSON.parse(await fs.promises.readFile('./data/movies.json', { encoding: 'utf8' }));
+    let counter = 0;
+    for (let movie of movies) {
+        counter++;
+        if (movie.BoxOffice === 'N/A' || !movie.BoxOffice) {
+            movie.Price = 10;
+        } else {
+            let price = Math.ceil(Math.random() * (45 - 30) + 30);
+            movie.Price = price;
+        }
+        console.log(`${movie.BoxOffice} - ${movie.Price}`);
+    }
+    console.log(`${counter} movies have been added Price attribute`);
+    await fs.promises.writeFile('./data/movies.json', JSON.stringify(movies, null, 2));
+});
 
 
 app.listen('3002', () => {
